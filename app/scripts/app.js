40eb7c9fa9bb6e01d9d994614cf2f182d8497d20
Michael Mandato <michaelmandato@dreamfactory.com>
2014-04-14 11:55:23 -0400

    Update constants to reflect tutorial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,11 @@ angular
         'ngRoute',
         'dfUserManagement'
     ])
-    .constant('DSP_URL', 'http://10.0.0.5:8082')
-    .constant('DSP_API_KEY', 'admin')
+
+    // Remember to set your DSP URL.  It will be different then
+    // the on listed here because it is specific to your DSP.
+    .constant('DSP_URL', 'http://192.168.1.16:8082')
+    .constant('DSP_API_KEY', 'dfauth')
     .config(['$httpProvider', 'DSP_API_KEY', function ($httpProvider, DSP_API_KEY) {
 
         // Set default headers for http requests
